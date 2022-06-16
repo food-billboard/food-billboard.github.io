f8f1e0e150838c3388a0635878eda0263909c7d8
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-06-16 16:04:03 +0800

    chore(): 减少卡顿配置

--- scripts/star.js
+++ scripts/star.js
@@ -1,14 +1,12 @@
 
-// 动态彩带
+// 动态彩带 <script src="//cdn.jsdelivr.net/gh/bynotes/texiao/source/js/caidai.js"></script>
 // 动态黑色线条
 // 鼠标星星特效
-// 雪花特效
+// 雪花特效 <script src="//cdn.jsdelivr.net/gh/bynotes/texiao/source/js/daxuehua.js"></script>
 // 点击文字特效
 hexo.extend.injector.register('body_end', `
-  <script src="//cdn.jsdelivr.net/gh/bynotes/texiao/source/js/caidai.js"></script>
   <script src="//cdn.jsdelivr.net/gh/bynotes/texiao/source/js/xiantiao.js"></script>
   <script src="//cdn.jsdelivr.net/gh/bynotes/texiao/source/js/xiaoxingxing.js"></script>
-  <script src="//cdn.jsdelivr.net/gh/bynotes/texiao/source/js/daxuehua.js"></script>
   <script src="//cdn.jsdelivr.net/gh/bynotes/texiao/source/js/dianjichuzi.js"></script>
 `)
 
